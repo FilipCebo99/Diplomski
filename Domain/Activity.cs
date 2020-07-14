e96412f3a1d02fe4e85c161b66d50699662b507f
FilipCebo99 <filipcebo9@gmail.com>
2020-07-14 21:29:25 +0200

    End of Section 14

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 
 namespace Domain
 {
@@ -12,5 +12,7 @@ namespace Domain
         public DateTime Date { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+
+        public virtual ICollection<UserActivity> UserActivities { get; set; }
     }
 }
